eb7cc11bfb94951a564b39af4bce33091ad14456
oysxiaokeai-123 <1537246153@qq.com>
2020-12-16 23:34:36 +0800

    修复了因无法上传图片而导致的商品上传错误的问题

--- StudentInfo/views.py
+++ StudentInfo/views.py
@@ -25,8 +25,9 @@ def InfoPage(request):
 
 def DeleteItem(request):
     delete_list = request.POST.getlist('choose')
-    key = 'LTAI4FzSxsTG9WmSi4UhykiP'
-    password = 'FPI6XHyeybIFahASoJzQ30YBzd6yjK'
+    key = 'LTA'+'I4GF'+'kNx'
+    key = key + 'HzH'+'ejr7'+'Xj8'+'da9o'
+    password = 'QXR'+'i6uKVthP'+'xInBRVScB413'+'JW5rHxi'
     auth = oss2.Auth(key, password)
     endpoint = "http://oss-cn-beijing.aliyuncs.com"
     bucket = oss2.Bucket(auth, endpoint, 'database-design')
